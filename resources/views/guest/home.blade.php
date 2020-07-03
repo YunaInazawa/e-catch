c4a2cbd7b3a60cfb545ba74ac047d6e4f0bc5670
sei <teatime.0704@gmail.com>
2020-07-03 10:19:13 +0900

    [add] bootstrap

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,7 @@
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  
+  <button type="button" class="btn btn-primary">Primary</button>
