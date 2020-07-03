69313d64f1a12e8d1aee6dc88983794e4fba422f
YamaguchiMayuko <2170035@ecccomp.com>
2020-07-03 10:03:14 +0900

    ゲストホームに執拗だと思われるオブジェクトを追加。

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <img><p>それっぽいいらすと
+        </p>
+        <button>←</button>ここにぼやっとイベント表示<button>右</button><br>
+        <button></button><br>
+        <p>新規の方は<a>こちら</a>から</p>
+    </body>
+</html>
