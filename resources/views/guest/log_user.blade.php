9226fb4406d3e81a4bf2938028223e3589cb8c97
YamaguchiMayuko <2170035@ecccomp.com>
2020-07-07 11:13:25 +0900

    ゲストの会員ログイン画面に必要だと思われるオブジェクトを追加

---
+++ resources/views/guest/log_user.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<html>
+    <head>
+    </head>
+    <body>
+        <div class="container">
+            <p>Login</p>
+            <div class="card card-container">
+                <p></p>
+                <form>
+                    <div>メールアドレス</div>
+                    <input type="email" id="inputEmail" class="form-control" placeholder="Mail Address" autofocus>
+                    <br>
+                    <div>パスワード</div>
+                    <input type="password" id="inputPassword" class="form-control" placeholder="Password">
+                    <p>※ここにエラー文が入る←エラーが発生した時だけ表示させたい。</p>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
+                </form>
+                <p>会員登録してない方は<a href="{{ url('/regi_user') }}">こちら</a></p>
+            </div>
+            <a href="{{ url('/log_store') }}">店舗に切り替え</a>
+        </div>
+    </body>
+</html>
