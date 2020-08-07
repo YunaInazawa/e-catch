f9489708a4184f73dc212db9eca66d092451796c
YamaguchiMayuko <2170035@ecccomp.com>
2020-08-07 09:24:39 +0900

    会員、店舗のログイン、新規登録画面をデフォで作った画面のデザインに変えた

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,25 @@
-@extends('layouts.app')
-
+@extends('layouts.header_guest')
+<!DOCTYPE html>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('会員登録') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('名前　※必須') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="名前・ニックネーム可" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +30,19 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label class="col-md-4 col-form-label text-md-right">{{ __('性別') }}</label>
+                            <div class="col-md-6">
+                                <span><input type="radio" name="sex" value="女" autocomplete="sex">女
+                                <input type="radio" name="sex" value="男" autocomplete="sex">男
+                                <input type="radio" name="sex" value="その他" autocomplete="sex" checked>その他</span>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス　※必須') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  placeholder="メールアドレス" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -40,10 +53,26 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label class="col-md-4 col-form-label text-md-right">{{ __('誕生日　※必須') }}</label>
+                            <div class="col-md-6">
+                                <input type="date" class="form-control" name="birth_day" value="1990-01-01" autocomplete="birth_day">
+                                <p>※ここにエラー文が入る←エラーが発生した時だけ表示させたい。</p>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">{{ __('郵便番号　※必須') }}</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="postal_code" placeholder="999-9999" autocomplete="postal_code">
+                                <p>※ここにエラー文が入る←エラーが発生した時だけ表示させたい。</p>
+                            </div>
+                        </div>    
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="半角英数字" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -54,22 +83,26 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('パスワード確認') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="半角英数字" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <input type="hidden" name="postal_code" value="999-9999" autocomplete="postal_code">
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">{{ __('アイコン') }}</label>
+                            <div class="col-md-6">
+                            <input type="file" id="inputIcon">
+                            </div>
+                        </div>
+                        
                         <input type="hidden" name="street_addr" value="addr" autocomplete="street_addr">
-                        <input type="hidden" name="sex" value="man or woman" autocomplete="sex">
-                        <input type="hidden" name="birth_day" value="1998-07-14" autocomplete="birth_day">
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('登録') }}
                                 </button>
                             </div>
                         </div>
